f7ce24f81d3dbfcb752a5a4a782ee9657e73bc12
richardsmithds-byte <richardsmith.ds@gmail.com>
2026-05-06 23:58:56 +0400

    Add files via upload

---
+++ Task.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+   protected $fillable = ['name', 'user_id', 'completed', 'archived'];
+
+      public function user() {
+         return $this->belongsTo(User::class);
+      }
+}
